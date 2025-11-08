dbbf2a790553a7bd47080cf33dc71fe85a544169
Alexis <atinoco999@gmail.com>
2025-11-08 16:06:31 -0600

    Added child expanded details page to home and created html structure

--- hit-record/src/App.jsx
+++ hit-record/src/App.jsx
@@ -1,14 +1,15 @@
 import { Navigate, Route, Routes } from 'react-router'
 import './App.css'
-import Footer from './components/common/Footer'
 import Header from './components/common/Header'
-import HomePage from './components/pages/Home/HomePage'
-import ListeningLogPage from './components/pages/ListeningLogPage'
 import LoadingPage from './components/pages/LoadingPage'
+import HomePage from './components/pages/Home/HomePage'
+import ExpandedDetailsPage from './components/pages/Home/ExpandedDetailsPage'
+import ListeningLogPage from './components/pages/ListeningLogPage'
 import SearchPage from './components/pages/Search/SearchPage'
 import AboutPage from './components/pages/AboutPage'
-import BottomNavBar from './components/common/BottomNavBar'
 import ProfilePage from './components/pages/ProfilePage'
+import BottomNavBar from './components/common/BottomNavBar'
+import Footer from './components/common/Footer'
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route path = "/" element = {<LoadingPage />}/>
         <Route path = "/home" element = {<HomePage />}/>
+        <Route path = "/home/album" element = {<ExpandedDetailsPage />}/>
         <Route path = "/listening-log" element = {<ListeningLogPage />}/>
         <Route path = "/about" element = {<AboutPage />}/>
         <Route path = "/search" element = {<SearchPage />}/>
--- hit-record/src/components/pages/Home/ExpandedDetailsPage.jsx
+++ hit-record/src/components/pages/Home/ExpandedDetailsPage.jsx
@@ -1,9 +1,46 @@
+import { Link } from "react-router";
+import Card from "../../common/Card";
+import VinylRecord from "../../images/VinylRecord.png"
 
 const ExpandedDetailsPage = () => {
     return (
-        <>
-            
-        </>
+        <main>
+            <Link to="/home">
+            <h4 style={{textDecoration:"underline"}}>←Back to home</h4>
+            </Link>
+            <div className="expanded-album-content">
+                <Card>
+                    <img src={VinylRecord} className="placeholder"></img>
+                </Card>
+                <h4>Title</h4>
+                <h4>Artist</h4>
+                <Card className="rating-box">
+                    <span>★★★★★</span>
+                </Card>
+            </div>
+            <div className="review-content">
+                <div className="album-details-expanded">
+                    <Card className="content-box">
+                        <h4>[YEAR]</h4>
+                    </Card>
+                    <Card className="content-box">
+                        <h4>[GENRE]</h4>
+                    </Card>
+                    <Card className="content-box">
+                        <h4>[RUNTIME]</h4>
+                    </Card>
+                    <Card className="content-box">
+                        <h4>[# OF TRACKS]</h4>
+                    </Card>
+                </div>
+                <Card className="user-review">
+                    <h4>My Review:</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+                    </p>
+                </Card>
+            </div>
+        </main>
     );
 };
 
