5f6e15cb52b24b90dc029441e8a11e780287acd3
Alexis <atinoco999@gmail.com>
2025-11-08 16:22:57 -0600

    Added useEffect() logic to feetch API access token

--- hit-record/src/App.jsx
+++ hit-record/src/App.jsx
@@ -10,8 +10,37 @@ import AboutPage from './components/pages/AboutPage'
 import ProfilePage from './components/pages/ProfilePage'
 import BottomNavBar from './components/common/BottomNavBar'
 import Footer from './components/common/Footer'
+import { useEffect, useState } from 'react'
+
+
+// API User ID and Key for access
+const clientID = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
 function App() {
+const [accessToken, setAccessToken] = useState();
+
+  useEffect(() => {
+    // Storing the fetch parameters for readability
+    let authCredentials = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body:
+        "grant_type=client_credentials&client_id=" + clientID + "&client_secret=" + clientSecret,
+    };
+
+    // Fetch returns access token object with 3 key-values: access_token, token_type, and expires_in
+    fetch("https://accounts.spotify.com/api/token", authCredentials)
+      .then((response) => response.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+      }
+      );
+  }, []);
+
+  console.log(accessToken);
 
   return (
     <div id="body-container">
