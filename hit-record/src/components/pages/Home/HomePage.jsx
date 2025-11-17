80350ffd4ce3ab3c0cae9cd5d538ea4466772b3b
Alexis <atinoco999@gmail.com>
2025-11-16 18:23:06 -0600

    Added csearch page configuration to load albums and save state to review page

--- hit-record/src/components/pages/Home/HomePage.jsx
+++ hit-record/src/components/pages/Home/HomePage.jsx
@@ -3,7 +3,7 @@ import AlbumShelf from "../../common/AlbumShelf";
 import Card from "../../common/Card";
 import VinylRecord from "../../images/VinylRecord.png"
 
-const HomePage = () => {
+const HomePage = ({albumReviews}) => {
     return (
         <main>
             <h1><strong>My Favorites at a glance</strong></h1>
@@ -25,30 +25,15 @@ const HomePage = () => {
             <h1>Recently Listened →</h1>
             </Link>
             <AlbumShelf>
-                <Card className="album-card">
-                    <img src={VinylRecord} className="album-artwork"></img>
-                </Card>
-                <Card className="album-card">
-                    <img src={VinylRecord} className="album-artwork"></img>
-                </Card>
-                <Card className="album-card">
-                    <img src={VinylRecord} className="album-artwork"></img>
-                </Card>
-                <Card className="album-card">
-                    <img src={VinylRecord} className="album-artwork"></img>
-                </Card>
-                <Card className="album-card">
-                    <img src={VinylRecord} className="album-artwork"></img>
-                </Card>
-                <Card className="album-card">
-                    <img src={VinylRecord} className="album-artwork"></img>
-                </Card>
-                <Card className="album-card">
-                    <img src={VinylRecord} className="album-artwork"></img>
-                </Card>
-                <Card className="album-card">
-                    <img src={VinylRecord} className="album-artwork"></img>
-                </Card>
+                {albumReviews && albumReviews.length > 0 ? (albumReviews.map((album) => {
+                        return(
+                            <Card className="album-card">
+                                <img src={album.image} className="album-artwork"></img>
+                                </Card>
+                        )
+                    })) :
+                    (<p>Nothing to show here yet...</p>)
+                }
             </AlbumShelf>
         </main>
     );
