b4b5ed5414ec586b897b583a7942c11e2d3c83d5
Alexis <atinoco999@gmail.com>
2025-11-17 19:17:46 -0600

    Added dynamic updating counters to listening log page

--- hit-record/src/components/pages/ListeningLogPage.jsx
+++ hit-record/src/components/pages/ListeningLogPage.jsx
@@ -2,16 +2,26 @@ import AlbumShelf from "../common/AlbumShelf";
 import Card from "../common/Card";
 import VinylRecord from "../images/VinylRecord.png"
 
-const ListeningLogPage = () => {
+const ListeningLogPage = ({ albumReviews }) => {
+
+    const calculateAvg = (reviews) => {
+        let sum = reviews.reduce((accumulator, reviews) => accumulator + reviews.rating.length, 0);
+
+        let avg = (sum / reviews.length).toFixed(1);
+        return avg;
+    }
+
+
     return (
         <main>
             <h1><strong>My Listening Log</strong></h1>
             <section id="user-stats-log-section">
                 <Card className="listening-stat-card">
-                    <h2>[# of logged albums]</h2>
+                    <h2>{albumReviews.length === 1 ? (`${albumReviews.length} review`) : (`${albumReviews.length} reviews`)}</h2>
                 </Card>
                 <Card className="listening-stat-card">
-                    <h2>[avg rating]</h2>
+                    <h2>{albumReviews.length === 0 ? ("No average to display yet") : (`${calculateAvg(albumReviews)} average rating`)}
+                    </h2>
                 </Card>
             </section>
             <AlbumShelf className="recently-listened-shelf">
